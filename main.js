ebc81d47396a845f3eb57dbd9e41c665d9957ac6
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-10-12 18:55:07 +0600

    Built a basic app with h1 and p

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'Socks',
+            description: 'Solid: 77% Cotton, 18% Polyester, 3% Nylon, 2% Elastane; Blocked: 68% Cotton, 27% Polyester, 3% Nylon, 2% Elastane'
+        }
+    }
+})
