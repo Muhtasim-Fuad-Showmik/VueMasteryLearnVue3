b2a72ae502ea9e8cac1112e3f1baa4edb00db7c4
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-10-12 19:03:23 +0600

    Used attribute binding on url and img src

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const app = Vue.createApp({
     data: function() {
         return {
             product: 'Socks',
-            description: 'Solid: 77% Cotton, 18% Polyester, 3% Nylon, 2% Elastane; Blocked: 68% Cotton, 27% Polyester, 3% Nylon, 2% Elastane'
+            description: 'Solid: 77% Cotton, 18% Polyester, 3% Nylon, 2% Elastane; Blocked: 68% Cotton, 27% Polyester, 3% Nylon, 2% Elastane',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.google.com/'
         }
     }
 })
