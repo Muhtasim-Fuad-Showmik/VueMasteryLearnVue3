7b60dcb61f73cf4b0df52116ca3a06b3cc038392
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-10-13 17:38:12 +0600

    Implemented hover and click event handlers

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const app = Vue.createApp({
     data: function() {
         return {
+            cart: 0,
             product: 'Socks',
             description: 'Solid: 77% Cotton, 18% Polyester, 3% Nylon, 2% Elastane; Blocked: 68% Cotton, 27% Polyester, 3% Nylon, 2% Elastane',
             image: './assets/images/socks_green.jpg',
@@ -9,10 +10,21 @@ const app = Vue.createApp({
             onSale: true,
             details: ['50% cotton', '30% wool', '20%polyester'],
             variants: [
-                { id: 2234, color: 'green' },
-                { id: 2235, color: 'blue' },
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ],
             sizes: [ 'S', 'M' ]
+        }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        removeFromCart() {
+            this.cart -= 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
         }
     }
 });
