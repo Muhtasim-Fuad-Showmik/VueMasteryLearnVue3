0bbc0465272c8a38fe1886ceeb07d8a3e0eab8cb
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-10-13 12:32:32 +0600

    Implemented conditional rendering for sale & stock

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ const app = Vue.createApp({
             product: 'Socks',
             description: 'Solid: 77% Cotton, 18% Polyester, 3% Nylon, 2% Elastane; Blocked: 68% Cotton, 27% Polyester, 3% Nylon, 2% Elastane',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.google.com/'
+            url: 'https://www.google.com/',
+            inventory: 100,
+            onSale: true
         }
     }
 })
