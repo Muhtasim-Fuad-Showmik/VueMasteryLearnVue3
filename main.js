685dc47a0066d306a54e230fbc74cf87cef26a68
Muhtasim Fuad Showmik <muhtasimfuadshowmik@gmail.com>
2022-10-17 12:14:01 +0600

    Used event communication to fix add & delete btns

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 const app = Vue.createApp({
     data: function() {
         return {
-            cart: 0,
+            cart: [],
             premium: true
         }
     },
     methods: {
+        addIdToCart(id) {
+            this.cart.push(id);
+        },
+        removeIdFromCart(id) {
+            let indexOfDeletion = this.cart.indexOf(id);
+            if(indexOfDeletion >= 0) {
+                this.cart.splice(this.cart.indexOf(id), 1);
+            } else {
+                console.log("Item not in cart!");
+            }
+        }
     }
 });
